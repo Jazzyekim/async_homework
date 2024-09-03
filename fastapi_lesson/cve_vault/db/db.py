add8a180a4534c01a19687efb2015f3bee72b0d6
misha.dyrda <jazzyekim@gmail.com>
2024-09-03 19:46:16 +0200

    Use singleton to create db engine

--- fastapi_lesson/cve_vault/db/db.py
+++ fastapi_lesson/cve_vault/db/db.py
@@ -1,12 +1,25 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.ext.asyncio import AsyncAttrs
 from cve_vault.config import Settings
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+
+class DatabaseEngineSingleton:
+    _instance = None
+
+    def __new__(cls, settings):
+        if cls._instance is None:
+            cls._instance = super(DatabaseEngineSingleton, cls).__new__(cls)
+            cls._instance.engine = create_async_engine(settings.db_uri, echo=True)
+        return cls._instance
+
+    def get_engine(self):
+        return self.engine
 
 
 def get_engine(settings: Settings) -> AsyncSession:
-    from sqlalchemy.ext.asyncio import create_async_engine
-    return create_async_engine(settings.db_uri, echo=True)
+    return DatabaseEngineSingleton(settings).get_engine()
 
 
 class Base(DeclarativeBase, AsyncAttrs):
